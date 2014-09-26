1ce12a185bf90f372b29288802e43fadfbb36d3a
yangmeansyoung <yangmeansyoung@gmail.com>
2014-09-26 08:34:01 +0800

    add 200OK handler as hello world

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject config-service-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "restful configuration service"
+  :url "https://github.com/zacyang/config-service-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.3.0"]
+                 [ring/ring-jetty-adapter "1.3.0"]])
--- src/config_service_clj/core.clj
+++ src/config_service_clj/core.clj
@@ -1,6 +1,6 @@
 (ns config-service-clj.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
