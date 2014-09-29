cc59a79da945478aed4c5cbb314a2eabaabc6b95
yangmeansyoung <yangmeansyoung@gmail.com>
2014-09-29 09:02:50 +0800

    move ring-mock to dev dependency

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
-                 [ring-mock "0.1.5"]]
+                 ]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler config-service-clj.core/handler}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}}
   )
