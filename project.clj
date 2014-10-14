34afb0e67fdefc0ee522f25445a084d763eacd7e
yangmeansyoung <yangmeansyoung@gmail.com>
2014-10-14 16:28:55 +0800

    add security skeleton.

--- project.clj
+++ project.clj
@@ -11,8 +11,9 @@
                  [ring.middleware.logger "0.5.0"]
                  [com.ashafa/clutch "0.4.0"]
                  [com.cemerick/friend "0.2.1"]
+                 [ring-basic-authentication "1.0.5"]
                  ]
   :plugins [[lein-ring "0.8.11"]]
-  :ring {:handler service.handle/app}
+  :ring {:handler service.handle/secured-app}
   :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
   )
--- src/service/handle.clj
+++ src/service/handle.clj
@@ -1,11 +1,9 @@
 (ns service.handle
   (:require [service.route :as route]
+            [service.secure :as secure]
             [ring.middleware.json :as middleware]
             [compojure.handler :as handler]
             [ring.middleware.logger :as logger]
-            [cemerick.friend :as friend]
-            [cemerick.friend [workflows :as workflows]
-             [credentials :as creds]]
             ))
 
 (def app
@@ -16,28 +14,6 @@
     (logger/wrap-with-logger))
   )
 
-;(def secured-app
-;  (-> app
-;    (secure/secure-app)))
-
-
-
-(def ^:private validate-uses
-  {
-    "root" {
-             :username "admin"
-             :password (creds/hash-bcrypt "admin_password")
-             :roles #{::admin}
-             }
-    "dev" {
-            :username "dev"
-            :password (creds/hash-bcrypt "dev_password")
-            :roles #{::user}
-            }})
-
 (def secured-app
   (-> app
-    (friend/authenticate {:credential-fn (partial creds/bcrypt-credential-fn validate-uses)
-                          :workflows [(workflows/interactive-form)]})
-    ; ...required Ring middlewares ...
-    ))
+    (secure/secure-app)))
